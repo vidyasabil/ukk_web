0a6b75a47d94493ac79901aed09712d239d64846
vidyasabil <vidyashabil@gmail.com>
2018-01-19 19:40:16 +0700

    sprint 1 19/01/2018 1 & 2

---
+++ application/controllers/Login.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model('mlogin');
+	}
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('vlogin');
+	}
+	function aksi_login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => $password
+			);
+		$cek = $this->mlogin->cek_login("tb_user",$where)->num_rows();
+		if($cek > 0){
+ 
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+				);
+ 
+			$this->session->set_userdata($data_session);
+ 
+			redirect(base_url("admin"));
+ 
+		}else{
+			echo "Username dan password salah !";
+		}
+	}
+	function logout(){
+		$this->session->sess_destroy();
+		redirect(base_url('login'));
+	}
+}
+
---
+++ application/views/vlogin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="<?php echo base_url('Login/aksi_login'); ?>" method="post">
+  <div class="form-group"> 
+      <label for="pwd">username:</label>
+      <input type="text" name="username" class="form-control" id="pwd" placeholder="Enter username">
+    </div><br>
+	  <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" name="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div><br>
+      <button type="submit" class="btn btn-default">Submit</button>
+  
+</form>
+</body>
+</html>
